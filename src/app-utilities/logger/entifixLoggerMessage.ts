68f17b41951fecd700b54138b37129d5223f4738
Jason Diaz G <jasondiazg@gmail.com>
2020-07-11 22:02:33 -0600

    FIXED: description property documentation in entifix logger message.

--- src/app-utilities/logger/entifixLoggerMessage.ts
+++ src/app-utilities/logger/entifixLoggerMessage.ts
@@ -2,15 +2,15 @@
  * Logger Message structure.
  */
 interface EntifixLoggerMessage {
-    /** @descripion System Owner property contains the name of the system owner that trigger the log. */
+    /** @description System Owner property contains the name of the system owner that trigger the log. */
     systemOwner: string,
-    /** @descripion User property contains the name of the user that trigger the log. */
+    /** @description User property contains the name of the user that trigger the log. */
     user: string,
-    /** @descripion Message property contains the body text of the log. */
+    /** @description Message property contains the body text of the log. */
     message: string,
-    /** @descripion Origin property contains the name of the file that trigger the log. */
+    /** @description Origin property contains the name of the file that trigger the log. */
     origin?: string,
-    /** @descripion Developer property contains the name of the developer that code the function that trigger the log. */
+    /** @description Developer property contains the name of the developer that code the function that trigger the log. */
     developer?: string
 }
 
